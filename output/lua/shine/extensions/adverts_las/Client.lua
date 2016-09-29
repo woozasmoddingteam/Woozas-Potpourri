d163da0716f95814f4ee10f172fa47a25f67f208
Las <las@protonmail.ch>
2016-09-29 20:11:43 +0200

    Experimental commit, contains an advert-blocking method based on group

--- output/lua/shine/extensions/adverts_las/Client.lua
+++ output/lua/shine/extensions/adverts_las/Client.lua
@@ -1,18 +1,11 @@
 local Plugin = Plugin;
 
-local StringMessage = string.format("string (%i)", kMaxChatLength * 4 + 1);
-
-Shared.RegisterNetworkMessage("ADVERTS_LAS_ADVERT", {
-	pr = "integer (0 to 255)";
-	pg = "integer (0 to 255)";
-	pb = "integer (0 to 255)";
-	r = "integer (0 to 255)";
-	g = "integer (0 to 255)";
-	b = "integer (0 to 255)";
-	prefix = StringMessage;
-	message = StringMessage;
-});
+local unwanted = {"Hints"};
 
 Client.HookNetworkMessage("ADVERTS_LAS_ADVERT", function(msg)
+	local group = string.Explode(msg.group, "|");
+	for _, v in ipairs(group) do
+		if table.contains(unwanted, msg) then Shared.Message("Skipped an advert."); return nil; end;
+	end
 	Shine.AddChatText(msg.pr, msg.pg, msg.pb, msg.prefix, msg.r/255, msg.g/255, msg.b/255, msg.message);
 end);
--- output/lua/shine/extensions/adverts_las/Shared.lua
+++ output/lua/shine/extensions/adverts_las/Shared.lua
@@ -8,4 +8,18 @@ Plugin.Conflicts = {
 	}
 };
 
+local StringMessage = string.format("string (%i)", kMaxChatLength * 4 + 1);
+
+Shared.RegisterNetworkMessage("ADVERTS_LAS_ADVERT", {
+	pr = "integer (0 to 255)";
+	pg = "integer (0 to 255)";
+	pb = "integer (0 to 255)";
+	r = "integer (0 to 255)";
+	g = "integer (0 to 255)";
+	b = "integer (0 to 255)";
+	prefix = StringMessage;
+	message = StringMessage;
+	group = StringMessage;
+});
+
 Shine:RegisterExtension("adverts_las", Plugin);
