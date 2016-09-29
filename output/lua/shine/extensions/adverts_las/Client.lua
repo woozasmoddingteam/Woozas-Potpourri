0f909be4b3905383550386a4d661c3de623f1791
Las <las@protonmail.ch>
2016-09-29 20:47:06 +0200

    What works: !PrintNextAdvert, sh_disable_advert_group.
    The situation currently:
    The timer for some reason is not running, and you have to manually do
    !PrintNextAdvert. Also, the client-side Plugin.Initialise function is
    not getting run, so the client can only disable groups with
    sh_disable_advert_group "GroupName". Their disabled groups wont even
    be saved, so they have to do it every time they join.

--- output/lua/shine/extensions/adverts_las/Client.lua
+++ output/lua/shine/extensions/adverts_las/Client.lua
@@ -1,11 +1,16 @@
 local Plugin = Plugin;
 
-local unwanted = {"Hints"};
+local unwanted = {};
 
 Client.HookNetworkMessage("ADVERTS_LAS_ADVERT", function(msg)
 	local group = string.Explode(msg.group, "|");
 	for _, v in ipairs(group) do
-		if table.contains(unwanted, msg) then Shared.Message("Skipped an advert."); return nil; end;
+		if table.contains(unwanted, v) then Shared.Message("AdvertsLasClient: Skipped an advert."); return nil; end;
 	end
 	Shine.AddChatText(msg.pr, msg.pg, msg.pb, msg.prefix, msg.r/255, msg.g/255, msg.b/255, msg.message);
 end);
+
+Event.Hook("Console_sh_disable_advert_group", function(group)
+	table.insert(unwanted, group);
+end)
+--Plugin.BindCommand("sh_disable_advert", "DisableAdvert", Plugin.DisableAdvert, true, true);
