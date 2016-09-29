7d4a9127d3ce56d4f68e149dee461a02213f64dc
Las <las@protonmail.ch>
2016-09-29 16:33:16 +0200

    If a feature doesn't work, it's not a feature you wanted anyway. Serious: removed csay support. We don't use it anyway.

--- output/lua/shine/extensions/adverts_las/Server.lua
+++ output/lua/shine/extensions/adverts_las/Server.lua
@@ -3,7 +3,6 @@ local Shine = Shine
 local TableQuickCopy = table.QuickCopy
 local TableQuickShuffle = table.QuickShuffle
 local TableRemove = table.remove
-local type = type;
 local pairs = pairs;
 local ipairs = ipairs;
 local Plugin = Plugin;
@@ -16,7 +15,6 @@ local function parseAdverts(group, adverts, default)
 	local messages = {};
 
 	local template = {
-		type = adverts.Type or default.type;
 		prefix = adverts.Prefix or default.prefix;
 		pr = adverts.PrefixR or default.pr;
 		pg = adverts.PrefixG or default.pg;
@@ -24,8 +22,6 @@ local function parseAdverts(group, adverts, default)
 		r = adverts.R or default.r;
 		g = adverts.G or default.g;
 		b = adverts.B or default.b;
-		--position = (adverts.Position or default.position):lower();
-		position = "top";
 	};
 	if group then
 		template.group = {
@@ -39,12 +35,10 @@ local function parseAdverts(group, adverts, default)
 	adverts.Messages = adverts.Messages or {};
 	for _, v in ipairs(adverts.Messages) do
 		local message = {
-			type = template.type;
 			prefix = template.prefix;
 			pr = template.pr;
 			pg = template.pg;
 			pb = template.pb;
-			position = template.position;
 			r = template.r;
 			g = template.g;
 			b = template.b;
@@ -67,7 +61,6 @@ end
 Plugin.PrintNextAdvert = nil; -- This will be set to a function in Initialise.
 
 function Plugin:Initialise()
-	Shared.Message("Initialised My Plugin.");
 
 	local globalName = self.Config.GlobalName or "All";
 
@@ -76,11 +69,9 @@ function Plugin:Initialise()
 		pr = 255,
 		pg = 255,
 		pb = 255,
-		type = "chat",
 		r = 255,
 		g = 255,
 		b = 255,
-		position = "top",
 	});
 
 	local len = #adverts;
@@ -110,38 +101,10 @@ function Plugin:Initialise()
 
 		local msg = adverts[msg_id];
 
-		if msg.type == "chat" then
-			Shine:NotifyDualColour(nil,
-				msg.pr,	msg.pg,	msg.pb,	msg.prefix,
-				msg.r,	msg.g,	msg.b,	msg.message,
-				false
-			);
-		else
-			local pos = msg.position;
-
-			local px, py = 0.5, nil;
-
-			if pos == "bottom" then
-				py = 0.8;
-			else
-				py = 0.2;
-			end
-
-			Shine.ScreenText.Add(20, {
-				X = px,
-				Y = py,
-				Text = msg.message,
-				Duration = 7,
-				R = msg.r,
-				G = msg.g,
-				B = msg.b,
-				Alignment = 1,
-				Size = 2,
-				FadeIn = 1
-			});
-		end
-
-		msg_id = msg_id + 1;
+		Shine:NotifyDualColour(nil,
+			msg.pr,	msg.pg,	msg.pb,	msg.prefix,
+			msg.r,	msg.g,	msg.b,	msg.message,
+		);
 	end
 
 	self:SimpleTimer(self.Config.Interval, self.PrintNextAdvert);
