903747396499942f69832c3a64217a83c6f93e63
Las <noway@ok.questionmark>
2016-09-29 19:16:05 +0200

    Client-side doesn't get loaded for some reason

--- output/lua/shine/extensions/adverts_las/Client.lua
+++ output/lua/shine/extensions/adverts_las/Client.lua
@@ -1,5 +1,21 @@
 local Plugin = Plugin;
 
 function Plugin:Initialise()
+	Shared.Message("I AM HERE");
+	Shared.RegisterNetworkMessage("ADVERTS_LAS_ADVERT", {
+		pr = "integer (0 to 255)";
+		pg = "integer (0 to 255)";
+		pb = "integer (0 to 255)";
+		r = "integer (0 to 255)";
+		r = "integer (0 to 255)";
+		b = "integer (0 to 255)";
+		prefix = StringMessage;
+		message = StringMessage;
+	});
+
+	Client.HookNetworkMessage("ADVERTS_LAS_ADVERT", function(msg)
+		Shine.AddChatText(msg.pr, msg.pg, msg.pb, msg.prefix, msg.r/255, msg.g/255, msg.b/255, msg.message);
+	end);
+
 	return true;
 end
