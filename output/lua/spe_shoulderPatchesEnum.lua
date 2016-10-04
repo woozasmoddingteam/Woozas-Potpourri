d9d1d510c4563bab926a8756d967a548d3166e45
wooza42 <wooza@apheriox.com>
2016-10-04 02:15:59 +0200

    Merged ShoulderPatches from Zycar into Potpourri

---
+++ output/lua/spe_shoulderPatchesEnum.lua
@@ -0,0 +1,27 @@
+--[[
+ 	ShoulderPatchesExtra
+	ZycaR (c) 2016
+]]
+
+kEmptyPatchIndex = 0
+kEmptyPatchName = "None"
+
+kShoulderPatchNames = {
+    // "None" - Empty patch with index 0 (reserved)
+    "Approved", // Wooza approved patch
+    "ZycaR",    // ZycaR (reserved)
+    "Nalice",   // Nalice (reserved)
+    "Lerk",
+    "Dilligaf",
+    "karrot",
+    "wooza1",
+    "Redguy",
+    "Avenger",
+    "O.R.A. Playtest",  // Wooza's O.R.A Play-testing Event
+    "Pacific",
+    "Gretel",
+    "Yolo",
+    "NS2:Combat Event",
+    "Cactuar",
+	"Duck"
+}
