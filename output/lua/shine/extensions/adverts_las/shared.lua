cd0f9088ab48a85c4407eebd845965cba081cbac
Las <las@protonmail.ch>
2016-10-04 15:46:39 +0200

    adverts_las is now user friendly (i.e. dumbed down)

--- output/lua/shine/extensions/adverts_las/shared.lua
+++ output/lua/shine/extensions/adverts_las/shared.lua
@@ -1,5 +1,5 @@
 local Shine = Shine;
-local Plugin = {}
+local Plugin = {};
 Plugin.Version = "1.0";
 Plugin.PrintName = "AdvertsLas";
 Plugin.HasConfig = true;
@@ -14,27 +14,26 @@ Plugin.Conflicts = {
 	};
 };
 
-local StringMessage = string.format("string (%i)", kMaxChatLength * 2);
-local IntMessage = "integer (0 to " .. tostring(2^32-1) .. ")";
+local uint8 = "integer (0 to 255)";
 
 function Plugin:SetupDataTable()
-	self:AddDTVar(StringMessage, "ServerID", "");
+	self:AddDTVar("string (128)", "ServerID", "");
 	self:AddNetworkMessage("Advert", {
-		prefix = "string (32)";
-		group = "string (96)";
-		message = "string (256)";
-		pr = "integer (0 to 255)";
-		pg = "integer (0 to 255)";
-		pb = "integer (0 to 255)";
-		r = "integer (0 to 255)";
-		g = "integer (0 to 255)";
-		b = "integer (0 to 255)";
+		str = "string (256)";
+		group = "string (32)";
 	}, "Client");
 	self:AddNetworkMessage("RequestForGroups", {}, "Server");
-	self:AddNetworkMessage("GroupsPart", {
-		msg = StringMessage;
+	self:AddNetworkMessage("Group", {
+		name = "string (32)";
+		prefix = "string (32)";
+		pr = uint8;
+		pg = uint8;
+		pb = uint8;
+		r = uint8;
+		g = uint8;
+		b = uint8;
+		hidable = "boolean";
 	}, "Client");
-	self:AddNetworkMessage("GroupsEnd", {}, "Client");
 end
 
 Shine:RegisterExtension("adverts_las", Plugin);
