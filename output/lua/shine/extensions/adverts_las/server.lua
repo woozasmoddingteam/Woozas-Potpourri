522b36ce2dc79022a6f251b33b22adebb6ccac00
Las <noway@ok.questionmark>
2016-10-12 19:45:29 +0200

    Experimental adverts_las

--- output/lua/shine/extensions/adverts_las/server.lua
+++ output/lua/shine/extensions/adverts_las/server.lua
@@ -6,54 +6,46 @@ local pairs = pairs;
 local Plugin = Plugin;
 
 Plugin.DefaultConfig = {
-	Interval = 60;
 	RandomiseOrder = true;
 	ServerID = "Your (semi-) unique server ID.";
 	Adverts = {
 		Warnings = {
+			Randomise = false;
 			Prefix = "Warning!";
-			PrefixR = 255;
-			PrefixG = 0;
-			PrefixB = 0;
-			R = 176;
-			G = 127;
-			B = 0;
+			PrefixColor = {
+				255, 0, 0
+			};
+			Color = {
+				176, 127, 0
+			};
+			Hidable = true;
+			Interval = 60;
 			Messages = {
 				"message1",
 				"message2"
 			};
-			Hidable = true;
+		}
+		PowerHints = {
+			Randomise = true;
+			Prefix = "POWER!";
+			PrefixColor = {
+				255, 255, 255
+			};
+			Color = {
+				0, 0, 0
+			};
+			Hidable = false;
+			Interval = 20;
+			Messages = {
+				"Message of power 1!",
+				"Message of power 2!",
+				"Message of power 3!",
+			};
 		}
 	}
 }
 
--- Recursive function that does a deep traversal of the adverts.
-local function parseAdverts(adverts)
-	local messages = {};
-	local groups = {};
-
-	for group, value in pairs(adverts) do
-		table.insert(groups, {
-			name = group;
-			prefix = value.Prefix or "";
-			pr = value.PrefixR or 0;
-			pg = value.PrefixG or 0;
-			pb = value.PrefixB or 0;
-			r = value.R or 0;
-			g = value.G or 0;
-			b = value.G or 0;
-			hidable = value.Hidable or false;
-		});
-		for i = 1, #value.Messages do
-			table.insert(messages, {
-				str = value.Messages[i];
-				group = group;
-			});
-		end
-	end
-
-	return messages, groups;
-end
+local groups = {};
 
 function Plugin:Initialise()
 	local serverID = self.Config.ServerID;
@@ -62,51 +54,44 @@ function Plugin:Initialise()
 	end
 	self.dt.ServerID = serverID;
 
-	local configAdverts = self.Config.Adverts;
-	--if not configAdverts then return false, "No adverts to show!" end
-	assert(configAdverts);
-
-	local adverts, groups = parseAdverts(configAdverts);
-	local len = #adverts;
-	assert(len ~= 0);
-
-	local interval = self.Config.Interval or 60;
-
-	local msg_id_func;
-	local msg_id = 0;
-
-	if self.Config.RandomiseOrder then
-		msg_id_func = function()
-			if msg_id == len then
-				TableQuickShuffle(adverts);
-				msg_id = 0;
+	for k, v in pairs(self.Config.Adverts) do
+		assert(v.Interval, "Interval for " .. k .. " is missing!");
+		table.insert(groups, {
+			name = k;
+			prefix = v.Prefix or "";
+			pr = v.PrefixColor[1] or 255;
+			pg = v.PrefixColor[2] or 255;
+			pb = v.PrefixColor[3] or 255;
+			r = v.Color[1] or 255;
+			g = v.Color[2] or 255;
+			b = v.Color[3] or 255;
+			hidable = v.Hidable;
+		});
+		local msg_n = 1;
+		local randomise = v.Randomise or true;
+		local messages = TableQuickCopy(v.Messages);
+		local len = #messages;
+		local func = function()
+			if msg_n == len then
+				msg_n = 1;
+				TableQuickShuffle(messages);
 			end
+			local msg = messages[msg_n];
+			self:SendNetworkMessage(nil, "Advert", {
+				str = msg;
+				group = k;
+			}, true);
 		end
-	else
-		msg_id_func = function()
-			msg_id = msg_id % len;
-		end
-	end
-
-	local function printNextAdvert()
-		msg_id_func();
-		msg_id = msg_id + 1;
-
-		local msg = adverts[msg_id];
-		self:SendNetworkMessage(nil, "Advert", msg, true);
-	end
-
-	self:CreateTimer("Adverts timer", interval, -1, printNextAdvert);
-
-	--self:BindCommand("sh_print_next_advert", "PrintNextAdvert", printNextAdvert, true, true);
-
-	function self:ReceiveRequestForGroups(Client)
-		for i = 1, #groups do
-			self:SendNetworkMessage(Client, "Group", groups[i], true)
-		end
+		self:CreateTimer("Adverts timer " .. k, v.Interval, -1, func);
 	end
 
 	self.Enabled = true;
 
 	return true;
+end
+
+function Plugin:ReceiveRequestForGroups(Client)
+	for i = 1, #groups do
+		self:SendNetworkMessage(Client, "Group", groups[i], true)
+	end
 end
