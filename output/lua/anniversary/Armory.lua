71afdd61ac807f37bcec4bcd3908544667304f7d
Las <las@protonmail.ch>
2016-10-22 15:46:44 +0200

    Working on damage

--- output/lua/anniversary/Armory.lua
+++ output/lua/anniversary/Armory.lua
@@ -8,25 +8,14 @@
 
 Script.Load("lua/Mixins/ClientModelMixin.lua")
 Script.Load("lua/LiveMixin.lua")
-Script.Load("lua/PointGiverMixin.lua")
-Script.Load("lua/AchievementGiverMixin.lua")
 Script.Load("lua/GameEffectsMixin.lua")
-Script.Load("lua/SelectableMixin.lua")
-Script.Load("lua/FlinchMixin.lua")
-Script.Load("lua/LOSMixin.lua")
-Script.Load("lua/CorrodeMixin.lua")
-Script.Load("lua/ConstructMixin.lua")
-Script.Load("lua/ResearchMixin.lua")
-Script.Load("lua/CommanderGlowMixin.lua")
+--Script.Load("lua/ConstructMixin.lua")
 
 Script.Load("lua/ScriptActor.lua")
 Script.Load("lua/RagdollMixin.lua")
 Script.Load("lua/ObstacleMixin.lua")
 Script.Load("lua/UnitStatusMixin.lua")
 Script.Load("lua/DissolveMixin.lua")
-Script.Load("lua/PowerConsumerMixin.lua")
-Script.Load("lua/InfestationTrackerMixin.lua")
-Script.Load("lua/SupplyUserMixin.lua")
 Script.Load("lua/IdleMixin.lua")
 
 class 'WoozArmory' (ScriptActor)
@@ -43,17 +32,10 @@ AddMixinNetworkVars(BaseModelMixin, networkVars)
 AddMixinNetworkVars(ClientModelMixin, networkVars)
 AddMixinNetworkVars(LiveMixin, networkVars)
 AddMixinNetworkVars(GameEffectsMixin, networkVars)
-AddMixinNetworkVars(FlinchMixin, networkVars)
 AddMixinNetworkVars(TeamMixin, networkVars)
-AddMixinNetworkVars(LOSMixin, networkVars)
-AddMixinNetworkVars(CorrodeMixin, networkVars)
-AddMixinNetworkVars(ConstructMixin, networkVars)
-AddMixinNetworkVars(ResearchMixin, networkVars)
-AddMixinNetworkVars(SelectableMixin, networkVars)
 
 AddMixinNetworkVars(ObstacleMixin, networkVars)
 AddMixinNetworkVars(DissolveMixin, networkVars)
-AddMixinNetworkVars(PowerConsumerMixin, networkVars)
 AddMixinNetworkVars(IdleMixin, networkVars)
 
 function WoozArmory:OnCreate()
@@ -64,24 +46,11 @@ function WoozArmory:OnCreate()
     InitMixin(self, ClientModelMixin)
     InitMixin(self, LiveMixin)
     InitMixin(self, GameEffectsMixin)
-    InitMixin(self, FlinchMixin)
     InitMixin(self, TeamMixin)
-    InitMixin(self, PointGiverMixin)
-    InitMixin(self, AchievementGiverMixin)
-    InitMixin(self, SelectableMixin)
     InitMixin(self, EntityChangeMixin)
-    InitMixin(self, LOSMixin)
-    InitMixin(self, CorrodeMixin)
-    InitMixin(self, ConstructMixin)
-    InitMixin(self, ResearchMixin)
     InitMixin(self, RagdollMixin)
     InitMixin(self, ObstacleMixin)
     InitMixin(self, DissolveMixin)
-    InitMixin(self, PowerConsumerMixin)
-
-    if Client then
-        InitMixin(self, CommanderGlowMixin)
-    end
 
     self:SetLagCompensated(false)
     self:SetPhysicsType(PhysicsType.Kinematic)
@@ -95,21 +64,12 @@ function WoozArmory:OnInitialized()
     self:SetModel(WoozArmory.kModelName, kAnimationGraph)
 
     if Server then
-
         InitMixin(self, StaticTargetMixin)
-        InitMixin(self, InfestationTrackerMixin)
-        InitMixin(self, SupplyUserMixin)
-
     elseif Client then
-
-        InitMixin(self, UnitStatusMixin)
-        InitMixin(self, HiveVisionMixin)
-
-    end
+		InitMixin(self, UnitStatusMixin); -- to be removed; removes health bar
+	end
 
     InitMixin(self, IdleMixin)
-
-	self:SetConstructionComplete();
 
 	self:SetPoseParam("log_n", 0);
 	self:SetPoseParam("log_e", 0);
@@ -120,6 +80,11 @@ function WoozArmory:OnInitialized()
 	self:SetPoseParam("scan_e", 0);
 	self:SetPoseParam("scan_w", 0);
 	self:SetPoseParam("scan_s", 0);
+
+	local mask = bit.bor(kRelevantToTeam1Unit, kRelevantToTeam2Unit);
+	self:SetExcludeRelevancyMask(mask);
+
+	self:SetTeamNumber(kTeamInvalid);
 end
 
 function WoozArmory:GetCanBeUsed(player, useSuccessTable) end
@@ -127,8 +92,13 @@ function WoozArmory:GetCanBeUsedConstructed(byPlayer)
     return true;
 end
 
-function WoozArmory:GetRequiresPower()
-    return true;
+local can_die = false;
+function WoozArmory:GetCanDie()
+	return can_die;
+end
+
+function WoozArmory:GetCanTakeDamage()
+	return false;
 end
 
 Shared.RegisterNetworkMessage("WoozArmoryFound");
