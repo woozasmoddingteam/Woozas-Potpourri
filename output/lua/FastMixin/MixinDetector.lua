68901bbb230d40a6da44b16e7ec0f0bb6db7cc3c
Lass Safin <lasssafin@gmail.com>
2016-11-18 20:03:35 +0100

    Does each userdata have their own metatable?

--- output/lua/FastMixin/MixinDetector.lua
+++ output/lua/FastMixin/MixinDetector.lua
@@ -22,27 +22,89 @@ end
 
 -- The amount of times a mixin has to be instantiated in a row to be deemed inlinable.
 -- A single fail will blacklist it.
-local kMixinInliningLimit = 5;
+local kMixinInliningLimit = 3;
+
+local function hookMixinDetector(old)
+	local mixin_state = {}; -- Keeps track of the mixins
+	return function(self)
+		old(self);
+		local mixins = getmetatable(self).mixins;
+		for i = 1, #mixins do
+			local mixin = mixins[i];
+			if mixin_state[mixin] == nil then
+				mixin_state[mixin] = 0;
+			elseif mixin_state[mixin] ~= false then
+				mixin_state[mixin] = (mixin_state[mixin] or 0) + 1;
+			end
+		end
+		local previous = nil;
+		local prev_mixin = nil;
+		for mixin, count in pairs(mixin_state) do
+			if not previous then
+				previous = count;
+				prev_mixin = mixin;
+			else
+				if count > previous then
+					mixin_state[prev_mixin] = false;
+				elseif count < previous then
+					mixin_state[mixin] = false;
+				end
+
+				if count >= kMixinInliningLimit then
+					for mixin, value in pairs(mixin_state) do
+						if value then
+							InitMixinForClass(cls, mixin);
+						end
+					end
+					cls.OnCreate = old;
+					break;
+				end
+			end
+		end
+	end
+end
 
 function BeginMixinDetection()
 	for i = 1, #classes do
 		local cls = classes[i];
 		if cls.OnCreate then
-			local mixins = {}; -- Keeps track of the mixins
+			local mixin_state = {}; -- Keeps track of the mixins
 			local old = cls.OnCreate;
 			cls.OnCreate = function(self)
 				old(self);
-				local imixins = getmetatable(self).instance_mixins;
-				for i = 1, #imixins do
-					local mixin = imixins[i];
-					mixins[mixin] = mixins[mixin] + 1;
+				local mixins = getmetatable(self).mixins;
+				for i = 1, #mixins do
+					local mixin = mixins[i];
+					if mixin_state[mixin] == nil then
+						mixin_state[mixin] = 0;
+					elseif mixin_state[mixin] ~= false then
+						mixin_state[mixin] = (mixin_state[mixin] or 0) + 1;
+					end
 				end
 				local previous = nil;
-				for mixin, count in pairs(mixins) do
+				local prev_mixin = nil;
+				for mixin, count in pairs(mixin_state) do
 					if not previous then
 						previous = count;
+						prev_mixin = mixin;
 					else
-						if 
+						if count > previous then
+							mixin_state[prev_mixin] = false;
+						elseif count < previous then
+							mixin_state[mixin] = false;
+						end
+
+						if count >= kMixinInliningLimit then
+							for mixin, value in pairs(mixin_state) do
+								if value then
+									InitMixinForClass(cls, mixin);
+								end
+							end
+							cls.OnCreate = old;
+							break;
+						end
+					end
+				end
 			end
 		end
 	end
--- output/lua/FastMixin/Server.lua
+++ output/lua/FastMixin/Server.lua
@@ -1,6 +1,12 @@
 Log("DISABLING CLASS GENERATION");
 class = nil;
 
-Script.Load("lua/FastMixin/MixinDetector.lua");
+--Script.Load("lua/FastMixin/MixinDetector.lua");
 
-BeginMixinDetection();
+--BeginMixinDetection();
+
+local old = Entity.OnInitialised;
+Entity.OnInitialised = function(self)
+	old(self);
+	Log("Metatable: %s", tostring(getmetatable(self)));
+end
