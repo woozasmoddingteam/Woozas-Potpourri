cfae74a72bf7f08a7b8f99003b8cbcf0142538a1
Lass Safin <lasssafin@gmail.com>
2016-11-19 11:57:13 +0100

    Don't use yet.

--- output/lua/FastMixin/FileHooks.lua
+++ output/lua/FastMixin/FileHooks.lua
@@ -1,4 +1,2 @@
-Script.Load("lua/FastMixin/MixinDetector.lua");
-
 ModLoader.SetupFileHook("lua/MixinUtility.lua", "lua/FastMixin/MixinUtility.lua", "replace");
 ModLoader.SetupFileHook("lua/MixinDispatcherBuilder.lua", "", "halt");
--- output/lua/FastMixin/MixinDetector.lua
+++ output/lua/FastMixin/MixinDetector.lua
@@ -1,65 +1,4 @@
-local oldclass = class;
-
-local reg = debug.getregistry();
-local classes = reg.__CLASSES;
-if not classes then
-	classes = {};
-	reg.__CLASSES = classes;
-end
-
-local metatable = {
-	__index = function(self, key)
-		if self[key] ~= nil then
-			return self[key];
-		else
-			return self.__class[key];
-		end
-	end
-}
-
-local function GetMixinConstants(self)
-	return self.__mixindata
-end
-
-local function GetMixinConstant(self, constantName)
-	return self.__mixindata[constantName]
-end
-
-class = function(name)
-	local oldbasesetter = oldclass(name);
-	local cls = _G[name];
-	local meta = getmetatable(cls);
-	meta.name = name;
-	classes[#classes+1] = cls;
-
-	cls.__class_mixins = {};
-	cls.__class_mixintypes = {};
-	cls.__class_mixindata = {};
-	cls.GetMixinConstants = GetMixinConstants;
-	cls.GetMixinConstant = GetMixinConstant;
-
-	local old = meta.__newindex;
-	meta.__newindex = function(self, key, value)
-		old(self, key, value);
-		if key == "OnCreate" then
-			Log("Overriding OnCreate for %s!", name);
-			local old_OnCreate = cls.OnCreate; -- In case the original __newindex modifies it
-			cls.OnCreate = function(self)
-				old_OnCreate(self);
-				self.__mixins = {};
-				self.__mixintypes = setmetatable({__class = cls.__class_mixintypes}, metatable);
-				self.__mixindata = setmetatable({__class = cls.__class_mixindata}, metatable);
-				Log("Better ONCREATE! %s %s %s", name, tostring(self), self.__mixins);
-			end
-			meta.__newindex = old; -- Unhook ourselves when done
-		end
-	end
-
-	return function(base)
-		meta.base = base;
-		oldbasesetter(base);
-	end
-end
+local classes = debug.getregistry().__CLASSES:
 
 -- The amount of times a mixin has to be instantiated in a row to be deemed inlinable.
 -- A single fail will blacklist it.
@@ -76,6 +15,9 @@ local function hookMixinDetector(old, callback)
 			return;
 		else
 			Log("Valid instance %s!", self);
+		end
+		if #mixins == 0 then -- If no mixins are used, we shouldn't wait.
+			callback(mixin_state, old);
 		end
 		for i = 1, #mixins do
 			local mixin = mixins[i];
@@ -111,8 +53,9 @@ function BeginMixinDetection()
 	for i = 1, #classes do
 		local cls = classes[i];
 		if cls.OnCreate then
+			Log("cls.OnCreate for %s: %s", getmetatable(cls).name, cls.OnCreate);
 			local callback = function(mixin_state, original)
-				Log("Done inlining for class %s!", getmetatable(cls).name)
+				Log("Done inlining in OnCreate for class %s!", getmetatable(cls).name)
 				cls.OnCreate = original;
 				for mixin, value in pairs(mixin_state) do
 					if value then
@@ -125,7 +68,7 @@ function BeginMixinDetection()
 		end
 		if cls.OnInitialized then
 			local callback = function(mixin_state, original)
-				Log("Done inlining for class %s!", getmetatable(cls).name)
+				Log("Done inlining in OnInitialized for class %s!", getmetatable(cls).name)
 				cls.OnInitialized = original;
 				for mixin, value in pairs(mixin_state) do
 					if value then
