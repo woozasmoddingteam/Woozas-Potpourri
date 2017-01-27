f01c0c8e9364cccbf9ba66e56ec4cbe5cf6725b8
Lass Safin <lasssafin@gmail.com>
2017-01-27 17:25:17 +0100

    Badges+ code was removed. Badges+ badges are still remaining.

--- output/lua/BadgesPlus/Badges+_Shared.lua
+++
@@ -1,4 +0,0 @@
-ModLoader.SetupFileHook( "lua/menu/GUIMainMenu_BadgesSelection.lua", "lua/BadgesPlus/GUIMainMenu_BadgesSelection.lua", "replace" )
-ModLoader.SetupFileHook( "lua/Badges_Shared.lua", "lua/BadgesPlus/Badges_Shared.lua", "replace" )
-ModLoader.SetupFileHook( "lua/Badges_Server.lua", "lua/BadgesPlus/Badges_Server.lua", "replace" )
-ModLoader.SetupFileHook( "lua/Badges_Client.lua", "lua/BadgesPlus/Badges_Client.lua", "replace" )
