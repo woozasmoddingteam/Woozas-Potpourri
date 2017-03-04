1a7afcc8412d24a53f97243b05c0d842f9d6f8ae
Las <las@protonmail.ch>
2017-03-04 12:06:40 +0100

    Updated PregamePeace and Secret Gorges
    Secret Gorges were conflicting with another mod
    PregamePeace now disallows aliens from being very near the CC, and allows Marines to enter the CC in pregame, just by pressing E anywhere on the body.

--- output/lua/PregamePeace/FileHooks.lua
+++ output/lua/PregamePeace/FileHooks.lua
@@ -1,1 +1,2 @@
 ModLoader.SetupFileHook("lua/NS2Gamerules.lua", "lua/PregamePeace/NS2Gamerules.lua", "post");
+ModLoader.SetupFileHook("lua/CommandStation.lua", "lua/PregamePeace/CommandStation.lua", "post");
