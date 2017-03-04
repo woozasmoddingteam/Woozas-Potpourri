53bc26ea6fb33a60be2fd16d37ea3d44b1192e06
Las <las@protonmail.ch>
2017-03-04 12:08:33 +0100

    Hotfix PregamePeace

--- output/lua/PregamePeace/CommandStation.lua
+++ output/lua/PregamePeace/CommandStation.lua
@@ -24,6 +24,6 @@ end
 
 if Server then
 	function CommandStation:GetIsPlayerValidForCommander(player)
-	    return player ~= nil and player:isa("Marine") and CommandStructure.GetIsPlayerValidForCommander(self, player)
+	    return player ~= nil and player:isa("Marine") and (not GetGameInfoEntity():GetGameStarted() or self:GetIsPlayerInside(player)) and CommandStructure.GetIsPlayerValidForCommander(self, player)
 	end
 end
