eb7d4a489ae48007911dc76eddbe745271adc1e7
Las <las@protonmail.ch>
2017-03-07 19:34:36 +0100

    LocalTalk mod added

---
+++ output/lua/LocalTalk/FileHooks.lua
@@ -0,0 +1,4 @@
+ModLoader.SetupFileHook("lua/GUIVoiceChat.lua", "lua/LocalTalk/GUIVoiceChat.lua","post")
+ModLoader.SetupFileHook("lua/InputHandler.lua", "lua/LocalTalk/InputHandler.lua","post")
+ModLoader.SetupFileHook("lua/BindingsDialog.lua", "lua/LocalTalk/BindingsDialog.lua","post")
+ModLoader.SetupFileHook("lua/NS2Gamerules.lua", "lua/LocalTalk/NS2Gamerules.lua", "post")
