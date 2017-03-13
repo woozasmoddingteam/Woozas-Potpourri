afe41beb0e69c1d532fa2c91e0ce09d1d4d255f0
Brute <email.brute@gmail.com>
2017-03-13 22:42:52 +0100

    fix customization menu and cleanup some of las' mess
    also use lua-style comments

--- output/lua/spe/FileHooks.lua
+++ output/lua/spe/FileHooks.lua
@@ -5,4 +5,4 @@
 
 ModLoader.SetupFileHook( "lua/Player.lua", "lua/spe/Player.lua" , "post" )
 ModLoader.SetupFileHook( "lua/menu/MenuPoses.lua", "lua/spe/MenuPoses.lua" , "post" )
---ModLoader.SetupFileHook( "lua/menu/GUIMainMenu_Customize.lua", "lua/spe/CustomizePatches.lua" , "post" )
+ModLoader.SetupFileHook( "lua/menu/GUIMainMenu_Customize.lua", "lua/spe/CustomizePatches.lua" , "post" )
