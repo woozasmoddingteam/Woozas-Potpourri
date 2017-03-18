a14377362887659b3ebb58f166fb226df7d3698f
wooza42 <wooza@apheriox.com>
2017-03-18 14:35:14 +0100

    Added Custom Badges and Shoulder Patches Tutorials as Mod Panel

---
+++ source/lua/custom_badges_mod_panel/FileHooks.lua
@@ -0,0 +1,5 @@
+if AddModPanel then
+    local kCustomBadgesModPanel = PrecacheAsset("materials/custom_badges_mod_panel/custom_badges_mod_panel.material")
+    AddModPanel(kCustomBadgesModPanel, "http://apheriox.com/custom-badges-tutorial/")
+
+end
