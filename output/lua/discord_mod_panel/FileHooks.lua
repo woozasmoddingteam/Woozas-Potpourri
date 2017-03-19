293950bedc7ff91e33d0458774a528dc753de2a8
wooza42 <wooza@apheriox.com>
2017-03-19 13:26:12 +0100

    Added 4 Mod Panels: Custom Badges Tutorial, Shoulder Patches Tutorial, Steam Group and Discord

---
+++ output/lua/discord_mod_panel/FileHooks.lua
@@ -0,0 +1,5 @@
+if AddModPanel then
+    local kCustomBadgesModPanel = PrecacheAsset("materials/discord_mod_panel/discord_mod_panel.material")
+    AddModPanel(kCustomBadgesModPanel, "https://discord.gg/0v5uwb6jDlQ7tKWs")
+
+end
