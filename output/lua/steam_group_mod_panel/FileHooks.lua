c4a871447726b6db24a213e38cf9bb06f4dbef61
wooza42 <daniel.hobi@live.de>
2017-03-19 13:26:12 +0100

    Added 4 Mod Panels: Custom Badges Tutorial, Shoulder Patches Tutorial, Steam Group and Discord

---
+++ output/lua/steam_group_mod_panel/FileHooks.lua
@@ -0,0 +1,5 @@
+if AddModPanel then
+    local kSteamGroup = PrecacheAsset("materials/steam_group_mod_panel/steam_group_mod_panel.material")
+    AddModPanel(kSteamGroup, "https://steamcommunity.com/groups/Apheriox")
+
+end
