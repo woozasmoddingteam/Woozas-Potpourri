af1721ce8782f76778d388a3d9632b159164ab26
Las <las@protonmail.ch>
2017-03-20 16:24:24 +0100

    Third implementation of random spawns

--- output/lua/quickload/woozapanels.lua
+++ output/lua/quickload/woozapanels.lua
@@ -3,21 +3,25 @@ if AddModPanel then
 	AddModPanel {
 		material = "materials/discord_mod_panel/discord_mod_panel.material",
 		url      = "https://discord.gg/0v5uwb6jDlQ7tKWs",
-		size     = {0.5, 0.5}
+		size     = {0.5, 0.5},
+		name     = "discord"
 	}
 	AddModPanel {
 		material = "materials/custom_badges_mod_panel.material",
 		url      = "http://apheriox.com/custom-badges-tutorial/",
-		size     = {0.5, 0.5}
+		size     = {0.5, 0.5},
+		name     = "custom_badges"
 	}
 	AddModPanel {
 		material = "materials/shoulder_patches_mod_panel.material",
 		url      = "http://apheriox.com/shoulder-patches-tutorial/",
-		size     = {0.5, 0.5}
+		size     = {0.5, 0.5},
+		name     = "shoulder_patches"
 	}
 	AddModPanel {
 		material = "materials/steam_group_mod_panel/steam_group_mod_panel.material",
 		url      = "https://steamcommunity.com/groups/Apheriox",
-		size     = {0.5, 0.5}
+		size     = {0.5, 0.5},
+		name     = "steam_group"
 	}
 end
