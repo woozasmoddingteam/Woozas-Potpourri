aa955d9f0349268ceb4d976b12365165b760d869
Las <las@protonmail.ch>
2017-03-28 13:38:19 +0200

    SYMBOLIC LINKS

--- synchroniser.d
+++ synchroniser.d
@@ -2,17 +2,34 @@
 
 import std.stdio;
 import std.file;
+import std.path;
+import std.array;
+import std.process;
+import core.stdc.stdlib : exit;
 
 void main() {
-	auto dirs = dirEntries("submodules", SpanMode.shallow);
+	// Update submodules
+	if(auto err = spawnProcess(["git", "submodule", "update"]).wait) {
+		writefln("git returned error code %s!", err);
+		exit(1);
+	}
+		
 	// Files that need to be removed when re-running
-	//auto registered_files = File("registered_submodule_files.txt", "r+");
+	if(exists("registered_files.txt")) foreach(string file; readText("registered_files.txt").split('\n')) if(exists(file)) {
+		writefln("Removing file %s...", file);
+		file.remove;
+	}
 
 	// Mods that need to be merged
-	foreach(dir; dirs) {
+	foreach(string modfolder; dirEntries("submodules", SpanMode.shallow)) {
 		// Look for output folder
-		foreach(subdir; dir.dirEntries) {
-
+		string output = modfolder;
+		foreach(string subdir; modfolder.dirEntries(SpanMode.shallow)) {
+			if(subdir.isDir && subdir.baseName == "output") {
+				output = subdir;
+				break;
+			}
 		}
+		writefln("Output folder for %s: %s", modfolder, output);
 	}
 }
