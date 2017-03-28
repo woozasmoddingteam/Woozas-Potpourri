d24a490b145415c0d593eeadcbd59501c25cd1ca
Las <las@protonmail.ch>
2017-03-28 19:43:19 +0200

    Automatic synchronisation with the synchroniser.d script
    The output folder is no longer part of git.
    Remember to synchronise before publishing to workshop!

--- synchroniser.d
+++ synchroniser.d
@@ -4,39 +4,44 @@ import std.stdio;
 import std.file;
 import std.path;
 import std.string;
+import std.array;
 import std.process;
 import core.stdc.stdlib : exit;
 
 void main() {
 	// Update submodules
-	if(auto err = spawnProcess(["git", "submodule", "update"]).wait) {
+	foreach(cmd; ["init", "update"]) if(auto err = spawnProcess(["git", "submodule", cmd]).wait) {
 		stderr.writefln("git returned error code %s!", err);
 		exit(1);
 	}
 	
 	if(exists("output"))
-		rmdir("output");
+		rmdirRecurse("output");
 	mkdir("output");
 
 	// Mods that need to be merged
-	foreach(string modfolder; dirEntries("submodules", SpanMode.shallow)) if(modfolder.baseName != "self") {
+	foreach(string modfolder; dirEntries("submodules", SpanMode.shallow)) {
 		// Look for output folder
 		string output = modfolder;
-		foreach(string subdir; modfolder.dirEntries(SpanMode.shallow)) {
-			if(subdir.isDir && subdir.baseName == "output") {
-				output = subdir;
-				break;
-			}
+		foreach(string entry; modfolder.dirEntries(SpanMode.shallow)) if(entry.isDir && entry.baseName == "output") {
+			output = entry;
 		}
 		output ~= '/';
 		writefln("Output folder for %s: %s", modfolder, output);
-		foreach(file; output.dirEntries(SpanMode.breadth)) {
-			writefln("%s         : %s", file.isDir ? "Dir " : "File", file);
-			auto path = chainPath("output", file.chompPrefix(output));
-			writefln("Relative path: %s", path);
-			if(file.isDir) {
-				path.mkdir;
+		foreach(string entry; output.dirEntries(SpanMode.breadth)) {
+			string path = chainPath("output", entry.chompPrefix(output)).array;
+			if(entry.baseName[0] == '.') {
+				writefln("Ignored %s!", path);
+			} else if(entry.isDir) {
+				if(!path.exists) path.mkdir;
+			} else if(path.exists) {
+				stderr.writefln("Already exists: %s!", path);
 			} else {
+				char[] relative_path;
+				foreach(i; 0 .. path.count('/'))
+					relative_path ~= "../";
+				relative_path ~= entry;
+				symlink(relative_path, path);
 			}
 		}
 	}
