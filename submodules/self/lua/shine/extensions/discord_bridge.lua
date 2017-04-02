3a6867f00c874c04f34fcd209f5c4da973589cb0
Las <las@protonmail.ch>
2017-04-02 19:09:57 +0200

    Changes made to discord_bridge plugin

--- submodules/self/lua/shine/extensions/discord_bridge.lua
+++ submodules/self/lua/shine/extensions/discord_bridge.lua
@@ -4,32 +4,27 @@ Plugin.NS2Only = false
 Plugin.HasConfig = true
 Plugin.ConfigName = "DiscordBridge.json"
 Plugin.DefaultConfig = {
-	address = "localhost:8080/discordbridge"
+	outbound = "localhost:64999/discordbridge_to"
+	inbound  = "localhost:64999/discordbridge_from"
 }
 
-local function discordSay(client, msg)
+local function fromDiscord(msg)
 	Shine:NotifyColour(nil, 50, 170, 120, msg)
+	Shared.SendHTTPRequest(self.inbound, "POST", fromDiscord)
 end
 
 function Plugin:Initialise()
 	self.botaddress = self.Config.address
-	local command = self:BindCommand("sh_discord_say", {}, discordSay, false, true)
-	command:AddParam {
-		Type = "string",
-		TakeRestOfLine = true
-	}
-
 	self.Enabled = true
+	Shared.SendHTTPRequest(self.inbound, "POST", fromDiscord)
+
 	return true
 end
 
 function Plugin:PlayerSay(client, message)
 	if not message.teamOnly then
-		Shared.SendHTTPRequest(self.botaddress, "POST", {
-            player = assert(client:GetControllingPlayer().name),
-            message  = assert(message.message),
-            server = "playground"
-        })
+		local name = client:GetControllingPlayer().name
+		Shared.SendHTTPRequest(self.outbound .. "?**" .. name .. "**: " .. message.message, "POST")
 	end
 end
 
