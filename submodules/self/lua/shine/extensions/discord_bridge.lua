34f8982bfd04437bfb5df733f2a60f7ab84ae2ea
Las <las@protonmail.ch>
2017-04-02 19:54:07 +0200

    Discord bridge is working, but needs polishing

--- submodules/self/lua/shine/extensions/discord_bridge.lua
+++ submodules/self/lua/shine/extensions/discord_bridge.lua
@@ -4,27 +4,33 @@ Plugin.NS2Only = false
 Plugin.HasConfig = true
 Plugin.ConfigName = "DiscordBridge.json"
 Plugin.DefaultConfig = {
-	outbound = "localhost:64999/discordbridge_to"
-	inbound  = "localhost:64999/discordbridge_from"
+	outbound = "localhost:64999",
+	inbound  = "localhost:64998"
 }
+
+local inbound
+local outbound
 
 local function fromDiscord(msg)
 	Shine:NotifyColour(nil, 50, 170, 120, msg)
-	Shared.SendHTTPRequest(self.inbound, "POST", fromDiscord)
+	Shared.SendHTTPRequest(inbound, "POST", fromDiscord)
 end
 
 function Plugin:Initialise()
-	self.botaddress = self.Config.address
+	inbound = self.Config.inbound
+	outbound = self.Config.outbound
 	self.Enabled = true
-	Shared.SendHTTPRequest(self.inbound, "POST", fromDiscord)
+	Shared.SendHTTPRequest(inbound, "POST", fromDiscord)
 
 	return true
 end
 
 function Plugin:PlayerSay(client, message)
 	if not message.teamOnly then
-		local name = client:GetControllingPlayer().name
-		Shared.SendHTTPRequest(self.outbound .. "?**" .. name .. "**: " .. message.message, "POST")
+		local player = client:GetControllingPlayer()
+		local name = player:GetName()
+		local team = player:GetTeam().name
+		Shared.SendHTTPRequest(outbound, "POST", {str = "__<" .. team .. ">__ **" .. name .. "**: " .. message.message})
 	end
 end
 
