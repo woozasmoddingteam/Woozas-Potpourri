8de93c3e6bb6c55f9482b946f47612d7320531b7
Las <las@protonmail.ch>
2017-10-27 20:33:05 +0200

    Fix shoulder patches

--- submodules/self/lua/spe/MenuPoses.lua
+++ submodules/self/lua/spe/MenuPoses.lua
@@ -5,16 +5,20 @@
 
 Script.Load("lua/spe/ShoulderPatchesConfig.lua")
 
-local ns2_Update = MenuPoses.Update
-function MenuPoses:Update(deltaTime)
-    ns2_Update(self, deltaTime)
+local old = MenuPoses.Update
+
+local model
+local function new(self, deltaTime)
+    old(self, deltaTime)
 
     -- set parameters to properly render menu poses patch
     local model = model.renderModel
     if MainMenu_GetIsOpened() and model ~= nil then
-	local player = Client.GetLocalPlayer()
-	local name, index = ShoulderPatchesConfig:GetClientShoulderPatch(player)
-	model:SetMaterialParameter("spePatchIndex", index)
-	model:SetMaterialParameter("spePatchEffect", player and player.spePatchEffect or 0)
+		local player = Client.GetLocalPlayer()
+		local name, index = ShoulderPatchesConfig:GetClientShoulderPatch(player)
+		model:SetMaterialParameter("spePatchIndex", index)
+		model:SetMaterialParameter("spePatchEffect", player and player.spePatchEffect or 0)
     end
 end
+
+debug.replacemethod("MenuPoses", "Update", new)
