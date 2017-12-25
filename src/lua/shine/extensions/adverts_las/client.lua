e4cb170e2f827b658f4c71cadd480e9bd0b3900c
Las <las@protonmail.ch>
2017-12-25 13:51:06 +0100

    Simplify advert configuration menu
    Too confusing for people

--- src/lua/shine/extensions/adverts_las/client.lua
+++ src/lua/shine/extensions/adverts_las/client.lua
@@ -3,24 +3,28 @@ local Plugin = Plugin;
 Plugin.DefaultConfig = {}
 
 local serverID;
-local should_hide;
 local groups = {};
 local hidable_group_count = 0;
 local config;
 
-local VoteMenuFunction = function() end;
-
 function Plugin:Initialise()
 	serverID = self.dt.ServerID;
-	--should_hide = self.Config[serverID] or false; -- Being nil also counts!
 	self.Config[serverID] = type(self.Config[serverID]) ~= "table" and {} or self.Config[serverID];
 	config = self.Config[serverID];
 
-	self:SendNetworkMessage("RequestForGroups", {}, true);
-
 	Shine.VoteMenu:EditPage("Main", function(self)
-		VoteMenuFunction(self);
+		self:AddSideButton("Advert Config", function()
+			self:SetPage("Advert Config");
+		end);
 	end);
+
+	Shine.VoteMenu:AddPage("Advert Config", function(self)
+		self:AddTopButton("[Cancel]", function()
+			self:SetPage("Main");
+		end);
+	end);
+
+	self:SendNetworkMessage("RequestForGroups", {}, true);
 
 	self.Enabled = true;
 	return true;
@@ -41,49 +45,19 @@ function Plugin:ReceiveAdvert(msg)
 end
 
 function Plugin:ReceiveGroup(msg)
+	Log("Received %s!", groups[i])
 	local name = msg.name;
 	groups[name] = msg;
 	config[name] = config[name] or false;
 	if msg.hidable then
-		hidable_group_count = hidable_group_count + 1;
-		if hidable_group_count > 2 then
-			Shine.VoteMenu:EditPage("Advert Config", function(self)
-				local str = (config[name] and "Show " or "Hide ") .. name;
-				self:AddSideButton(str, function()
-					config[name] = not config[name];
-					Plugin:SaveConfig();
-					self:SetIsVisible(false);
-					self:SetPage("Main");
-				end);
+		Shine.VoteMenu:EditPage("Advert Config", function(self)
+			local str = (config[name] and "Show " or "Hide ") .. name;
+			self:AddSideButton(str, function()
+				config[name] = not config[name];
+				Plugin:SaveConfig();
+				self:SetIsVisible(false);
+				self:SetPage("Main");
 			end);
-		elseif hidable_group_count == 2 then
-			Shine.VoteMenu:AddPage("Advert Config", function(self)
-				self:AddTopButton("[Cancel]", function()
-					self:SetPage("Main");
-				end);
-				local str = (config[name] and "Show " or "Hide ") .. name;
-				self:AddSideButton(str, function()
-					config[name] = not config[name];
-					Plugin:SaveConfig();
-					self:SetIsVisible(false);
-					self:SetPage("Main");
-				end);
-			end);
-			VoteMenuFunction = function(self)
-				self:AddSideButton("Advert Config", function()
-					self:SetPage("Advert Config");
-				end);
-			end
-		elseif hidable_group_count == 1 then
-			VoteMenuFunction = function(self)
-				local str = (config[name] and "Show " or "Hide ") .. name;
-
-				self:AddSideButton(str, function()
-					config[name] = not config[name];
-					Plugin:SaveConfig();
-					self:SetIsVisible(false);
-				end);
-			end
-		end
+		end);
 	end
 end
