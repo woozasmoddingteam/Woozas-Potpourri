6bb799a5d2b6530502d8bc98260d859f0f8b0e84
Las <las@protonmail.ch>
2018-04-09 21:28:59 +0200

    Make pregame peace a dependency

--- src/lua/PregamePeace/FileHooks.lua
+++
@@ -1,2 +0,0 @@
-ModLoader.SetupFileHook("lua/NS2Gamerules.lua", "lua/PregamePeace/NS2Gamerules.lua", "post");
-ModLoader.SetupFileHook("lua/CommandStation.lua", "lua/PregamePeace/CommandStation.lua", "post");
