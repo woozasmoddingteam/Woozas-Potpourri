b265b62f21f47b3e1410fda140d1dd3ea0459a8e
Las <las@protonmail.ch>
2018-07-08 17:04:39 +0200

    Make mine madness aka crazy mines a dependency instead of embedding it

--- src/lua/MineMadness/FileHooks.lua
+++
@@ -1,5 +0,0 @@
-
---if not string.find(Script.CallStack(), "Main.lua") then
-    ModLoader.SetupFileHook( "lua/Shared.lua", "lua/MineMadness/Shared.lua", "post" )
-    ModLoader.SetupFileHook( "lua/ServerAdminCommands.lua", "lua/MineMadness/ServerAdminCommands.lua", "post" )
---end
--- src/lua/MineMadness/Shared.lua
+++
@@ -1,1 +0,0 @@
-Script.Load("lua/MineMadness/LayMinesCrazy.lua")
