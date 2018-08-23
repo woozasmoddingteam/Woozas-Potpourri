b0a576dc93492a28e05a111420173ad9fd4af167
Las <las@protonmail.ch>
2018-08-23 19:15:22 +0200

    Fix super hades device model path

--- src/lua/Balance42/HadesDevice.lua
+++ src/lua/Balance42/HadesDevice.lua
@@ -1,1 +1,1 @@
-HadesDevice.kModelName = PrecacheAsset("/models/props/eclipse/eclipse_terminal01_01.model")
+HadesDevice.kModelName = PrecacheAsset("models/props/eclipse/eclipse_terminal01_01.model")
